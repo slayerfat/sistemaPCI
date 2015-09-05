5f008c35e57d340807e603cf6b0585c60d6d5a82
slayerfat <slayerfat@gmail.com>
2015-09-05 15:04:01 -0430

    poderoso copy/paste de git-bump

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,4 +1,48 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
 var elixir = require('laravel-elixir');
+
+// git bump (v0.1.2) <-- gulp release|feature|patch|pre
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp pre       # makes v0.1.0 → v0.1.1-0
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+
+function inc(importance) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        //.pipe(git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tag_version());
+}
+
+gulp.task('pre',     function() { return inc('prerelease'); });
+gulp.task('patch',   function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
 
 elixir(function(mix) {
     mix.sass('app.scss');
