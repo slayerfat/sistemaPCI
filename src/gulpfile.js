c87cb7b3feb8c1ac86accd97515579eb185e5d86
slayerfat <slayerfat@gmail.com>
2015-09-05 15:05:17 -0430

    activado commits.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -31,7 +31,7 @@ function inc(importance) {
         // save it back to filesystem
         .pipe(gulp.dest('./'))
         // commit the changed version number
-        //.pipe(git.commit('bumps package version'))
+        .pipe(git.commit('bumps package version'))
 
         // read only one file to get the version number
         .pipe(filter('package.json'))
