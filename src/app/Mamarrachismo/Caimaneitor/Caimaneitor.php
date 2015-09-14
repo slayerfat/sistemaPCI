3a1aa86a1dd3e17858950ea1ca68989faefd16e5
slayerfat <slayerfat@gmail.com>
2015-09-14 18:44:03 -0430

    good enough?

--- src/app/Mamarrachismo/Caimaneitor/Caimaneitor.php
+++ src/app/Mamarrachismo/Caimaneitor/Caimaneitor.php
@@ -6,6 +6,28 @@ use PCI\Mamarrachismo\Caimaneitor\Interfaces\CaimanizerInterface;
 class Caimaneitor implements CaimanizerInterface
 {
 
+    const CAIMANEISHONS = [
+        'Puro cara e\' papeo. - Bryan Torres',
+        'Tranquilo que en vacaciones me pongo pa\' eso. - Andres Leotur',
+        'Si, si... eso lo hago yo. - Erick Zerpa',
+        '¿Que tipo de relacion es una relacion de tipo muchos a muchos? - Anonimo',
+        '¿Cuanto es uno por menos uno? - Anonimo',
+        'Tuve que darle la vuelta al mundo '
+        .'para darme cuenta que la solucion estaba detras de mi. - Alejandro Granadillo',
+    ];
+
+    const LOCAISHONS = [
+
+        'Con cinco tragos de mas en la taguara La Barcaza Azul.',
+        'Tomando Roncito en Los Caracas.',
+        'Durante la prueba de calculo.',
+        'En el anden del Metro de Caracas.',
+        'Durante la Pre-defensa de Proyecto.',
+        'Via Skype.',
+        'Via Gmail.',
+
+    ];
+
     /**
      * Inspired by Illuminate\Foundation\Inspiring
      * A very Inspiring class.
@@ -16,41 +38,23 @@ class Caimaneitor implements CaimanizerInterface
      */
     public static function caimanais()
     {
-        return Collection::make([
-
-            'Puro cara e\' papeo. - Bryan Torres',
-            'Tranquilo que en vacaciones me pongo pa\' eso. - Andres Leotur',
-            'Si, si... eso lo hago yo. - Erick Zerpa',
-            '¿Que tipo de relacion es una relacion de tipo muchos a muchos? - Anonimo',
-            '¿Cuanto es uno por menos uno? - Anonimo',
-            'Tuve que darle la vuelta al mundo '
-            .'para darme cuenta que la solucion estaba detras de mi. - Alejandro Granadillo',
-
-        ])->random();
+        return Collection::make(self::CAIMANEISHONS)->random();
     }
 
     /**
-     * Inspired by Illuminate\Foundation\Inspiring
+     * Inspired by PCI\Mamarrachismo\Caimaneitor\Caimaneitor
      * A very Inspiring class.
      *
-     * Alejandro Granadillo made this commit from Caracas. (45° Celcius)
+     * Alejandro Granadillo made this commit from Caracas, Again.
      *
      * @return string
      */
     public static function locaishon()
     {
-        return Collection::make([
-
-            'Con cinco tragos de mas en la taguara La Barcaza Azul.',
-            'Tomando Roncito en Los Caracas.',
-            'Durante la prueba de calculo.',
-            'En el anden del Metro de Caracas.',
-            'Durante la Pre-defensa de Proyecto.',
-            'Via Skype.',
-            'Via Gmail.',
-
-        ])->random();
+        return Collection::make(self::LOCAISHONS)->random();
     }
+
+
 
     /**
      * @return string
