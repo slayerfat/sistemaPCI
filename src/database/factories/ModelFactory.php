4991e325b75227061734fdcf1c364bf1356d0c1b
slayerfat <slayerfat@gmail.com>
2015-09-14 14:42:43 -0430

    faker en español.

--- src/database/factories/ModelFactory.php
+++ src/database/factories/ModelFactory.php
@@ -4,13 +4,15 @@
 // tienen la misma estructura
 $models = PCI\Database\AuxEntitiesSeeder::getModels();
 
+$faker = Faker\Factory::create('es_ES');
+
 foreach ($models as $model) {
-    $factory->define($model, function (Faker\Generator $faker) {
+    $factory->define($model, function () use ($faker) {
         return ['desc' => $faker->word];
     });
 }
 
-$factory->define(PCI\Models\Address::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Address::class, function () use ($faker) {
     return [
         'parish_id' => 1,
         'building'  => $faker->buildingNumber,
@@ -19,7 +21,7 @@ $factory->define(PCI\Models\Address::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\Attendant::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Attendant::class, function () use ($faker) {
     return [
         'user_id'   => 1,
         'selection' => $faker->dateTime,
@@ -27,14 +29,14 @@ $factory->define(PCI\Models\Attendant::class, function (Faker\Generator $faker) 
     ];
 });
 
-$factory->define(PCI\Models\Department::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Department::class, function () use ($faker) {
     return [
         'desc'  => $faker->word,
         'phone' => $faker->phoneNumber,
     ];
 });
 
-$factory->define(PCI\Models\Depot::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Depot::class, function () use ($faker) {
     return [
         'user_id' => 1,
         'rack'    => rand(1, 5),
@@ -43,7 +45,7 @@ $factory->define(PCI\Models\Depot::class, function (Faker\Generator $faker) {
 });
 
 
-$factory->define(PCI\Models\Employee::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Employee::class, function () use ($faker) {
     return [
         'user_id'        => 1,
         'nationality_id' => 1,
@@ -59,7 +61,7 @@ $factory->define(PCI\Models\Employee::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\Item::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Item::class, function () use ($faker) {
     return [
         'sub_category_id' => rand(1, 2),
         'maker_id'        => rand(1, 2),
@@ -73,7 +75,7 @@ $factory->define(PCI\Models\Item::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\Movement::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Movement::class, function () use ($faker) {
     return [
         'note_id'          => 1,
         'movement_type_id' => 1,
@@ -81,7 +83,7 @@ $factory->define(PCI\Models\Movement::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\Note::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Note::class, function () use ($faker) {
     return [
         'user_id'      => 1,
         'attendant_id' => 1,
@@ -93,7 +95,7 @@ $factory->define(PCI\Models\Note::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\Petition::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\Petition::class, function () use ($faker) {
     return [
         'user_id'          => 1,
         'petition_type_id' => 1,
@@ -103,7 +105,7 @@ $factory->define(PCI\Models\Petition::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\User::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\User::class, function () use ($faker) {
     return [
         'profile_id'        => 2,
         'name'              => $faker->word,
@@ -115,7 +117,7 @@ $factory->define(PCI\Models\User::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(PCI\Models\WorkDetail::class, function (Faker\Generator $faker) {
+$factory->define(PCI\Models\WorkDetail::class, function () use ($faker) {
     return [
         'join_date'      => $faker->date,
         'departure_date' => $faker->date,
