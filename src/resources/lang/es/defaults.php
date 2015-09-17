f0b72191c72e9ce2083db768624139e283cb898a
slayerfat <slayerfat@gmail.com>
2015-09-17 19:24:55 -0430

    añadidos mensajes adicionales

--- src/resources/lang/es/defaults.php
+++ src/resources/lang/es/defaults.php
@@ -1,13 +1,60 @@
 <?php
 
 return [
-    'appName'    => 'sistemaPCI',
-    'auth.error' => 'Ud. no tiene permisos para esta acción.',
+    'appName'           => 'sistemaPCI',
+    'auth.error'        => 'Ud. no tiene permisos para esta acción.',
+
+    /**
+     * Categoria
+     */
+    'cats.index'        => 'Ver Categorias',
+    'cats.show'         => 'Ver Categoria',
+    'cats.create'       => 'Crear Categoria',
+    'cats.edit'         => 'Editar Categoria',
+    'cats.destroy'      => 'Eliminar Categoria',
+
+    /**
+     * Item
+     */
+    'items.index'       => 'Ver Items',
+    'items.show'        => 'Ver Item',
+    'items.create'      => 'Crear Item',
+    'items.edit'        => 'Editar Item',
+    'items.destroy'     => 'Eliminar Item',
+
+    /**
+     * Nota
+     */
+    'notes.index'       => 'Ver Notas',
+    'notes.show'        => 'Ver Nota',
+    'notes.create'      => 'Crear Nota',
+    'notes.edit'        => 'Editar Nota',
+    'notes.destroy'     => 'Eliminar Nota',
 
     /**
      * Usuario
      */
-    'users.show'    => 'Ver Cuenta',
-    'users.edit'    => 'Editar Cuenta',
-    'users.destroy' => 'Eliminar Cuenta',
+    'users.index'       => 'Ver Usuarios',
+    'users.show'        => 'Ver Cuenta',
+    'users.create'      => 'Crear Cuenta',
+    'users.edit'        => 'Editar Cuenta',
+    'users.destroy'     => 'Eliminar Cuenta',
+
+    /**
+     * Pedidos
+     */
+    'Petitions.index'   => 'Ver Pedidos',
+    'Petitions.show'    => 'Ver Pedido',
+    'Petitions.create'  => 'Crear Pedido',
+    'Petitions.edit'    => 'Editar Pedido',
+    'Petitions.destroy' => 'Eliminar Pedido',
+
+    /**
+     * Perfil
+     */
+    'profiles.index'    => 'Ver Perfiles',
+    'profiles.show'     => 'Ver Perfil',
+    'profiles.create'   => 'Crear Perfil',
+    'profiles.edit'     => 'Editar Perfil',
+    'profiles.destroy'  => 'Eliminar Perfil',
 ];
