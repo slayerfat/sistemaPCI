35a69008a883f7033f28cfbe012d3856ba56ecda
slayerfat <slayerfat@gmail.com>
2015-09-19 18:18:07 -0430

    divididos defaults en bloques mas logicos

---
+++ src/resources/lang/es/models.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    /**
+     * Item
+     */
+    'items.index'        => 'Ver Items',
+    'items.show'         => 'Ver Item',
+    'items.create'       => 'Crear Item',
+    'items.edit'         => 'Editar Item',
+    'items.destroy'      => 'Eliminar Item',
+    'items.plural'       => 'Items',
+    'items.singular'     => 'Item',
+
+    /**
+     * Nota
+     */
+    'notes.index'        => 'Ver Notas',
+    'notes.show'         => 'Ver Nota',
+    'notes.create'       => 'Crear Nota',
+    'notes.edit'         => 'Editar Nota',
+    'notes.destroy'      => 'Eliminar Nota',
+    'notes.plural'       => 'Notas',
+    'notes.singular'     => 'Nota',
+
+    /**
+     * Usuario
+     */
+    'users.index'        => 'Ver Usuarios',
+    'users.show'         => 'Ver Cuenta',
+    'users.create'       => 'Crear Cuenta',
+    'users.edit'         => 'Editar Cuenta',
+    'users.destroy'      => 'Eliminar Cuenta',
+    'users.plural'       => 'Cuentas',
+    'users.singular'     => 'Cuenta',
+
+    /**
+     * Pedidos
+     */
+    'Petitions.index'    => 'Ver Pedidos',
+    'Petitions.show'     => 'Ver Pedido',
+    'Petitions.create'   => 'Crear Pedido',
+    'Petitions.edit'     => 'Editar Pedido',
+    'Petitions.destroy'  => 'Eliminar Pedido',
+    'Petitions.plural'   => 'Pedidos',
+    'Petitions.singular' => 'Pedido',
+];
