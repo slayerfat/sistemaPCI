8c2dba10a2566e61dde0cd277dc5e053d06f79c2
slayerfat <slayerfat@gmail.com>
2015-09-19 16:59:05 -0430

    mas pruebas, falta completar aux y usuario.

---
+++ src/tests/Integration/Aux/AuxIntegrationTest.php
@@ -0,0 +1,53 @@
+<?php namespace Tests\Integration\Aux;
+
+use PCI\Models\Category;
+
+class AuxIntegrationTest extends AbstractAuxTest
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // como estas entidades son tan genericas, solo se necesita
+        // saber el nombre en la ruta ($model[0]), el alias
+        // ($model[1]) y la clase para crear nuevos
+        // registros y objetos ($model[2])
+        $modelData = [
+            ['categorias', 'cats', Category::class]
+        ];
+
+        foreach ($modelData as $model) {
+            $this->setData($model[0], $model[1], $model[2]);
+        }
+    }
+
+    public function testCategoryIndexShouldHaveTableWithValidInfo()
+    {
+        foreach ($this->data as $data) {
+            $this->actingAs($this->user)
+                ->visit($data->index)
+                ->see('No hay información que mostrar.');
+
+            $cat = factory($data->class)->create();
+
+            $this->actingAs($this->user)
+                ->visit($data->index)
+                ->see($cat->desc);
+        }
+    }
+
+    public function testCreateCategoryShouldReturnAForm()
+    {
+        foreach ($this->data as $data) {
+            $this->actingAs($this->user)
+                ->visit($data->index)
+                ->see('No hay información que mostrar.');
+
+            $this->actingAs($this->user)
+                ->visit('/')
+                ->click(trans("defaults.{$data->alias}.create"))
+                ->seePageIs($data->create);
+        }
+    }
+}
--- src/tests/Integration/Aux/CategoryIntegrationTest.php
+++ src/tests/Integration/Aux/CategoryIntegrationTest.php
@@ -1,23 +1,10 @@
 <?php namespace Tests\Integration\Aux;
 
-use PCI\Models\Category;
-use PCI\Models\User;
-use Tests\Integration\AbstractIntegrationTest;
-
-class CategoryIntegrationTest extends AbstractIntegrationTest
+class CategoryIntegrationTest extends AbstractAuxTest
 {
 
-    public function testCats()
+    public function testTodo()
     {
-        $user = factory(User::class)->create([
-            'confirmation_code' => null,
-            'profile_id' => User::ADMIN_ID
-        ]);
-
-        $this->actingAs($user)->visit('categorias')->see('No hay información que mostrar.');
-
-        $cat = factory(Category::class)->create();
-
-        $this->actingAs($user)->visit('categorias')->see($cat->desc);
+        $this->markTestIncomplete('faltan detalles');
     }
 }
