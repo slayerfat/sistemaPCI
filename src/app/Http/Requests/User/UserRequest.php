1d332cec15faa953394529936afcbcfbc8bc1fbc
slayerfat <slayerfat@gmail.com>
2015-09-23 16:01:18 -0430

    añadido comentarios

--- src/app/Http/Requests/User/UserRequest.php
+++ src/app/Http/Requests/User/UserRequest.php
@@ -25,7 +25,6 @@ class UserRequest extends Request
 
     /**
      * Determina si el usuario esta autorizado a hacer esta peticion.
-
      * @return bool
      */
     public function authorize()
@@ -41,26 +40,22 @@ class UserRequest extends Request
 
     /**
      * Obtiene las reglas de validacion que seran aplicadas a esta peticion.
-
      * @return array<string, string>
      */
     public function rules()
     {
-
-        /**
-         * Se necesita saber si el usuario esta o no actualizando algun recurso
-         * es por eso que se chequea si el metodo del formulario es patch
-         * o put (actualizacion), de ser asi se necesita cambiar un
-         * poco las reglas para permitir que los campos unicos
-         * se repitan solo para ese usuario, es decir
-         * name => required: excepto si mismo.
-         */
+        // Se necesita saber si el usuario esta o no actualizando algun recurso
+        // es por eso que se chequea si el metodo del formulario es patch
+        // o put (actualizacion), de ser asi se necesita cambiar un
+        // poco las reglas para permitir que los campos unicos
+        // se repitan solo para ese usuario, es decir
+        // name => required: excepto si mismo.
         switch ($this->method()) {
             case 'PUT':
             case 'PATCH':
                 return [
-                    'name'       => 'required|max:20|alpha-dash|unique:users,name,'.(int)$this->route('users'),
-                    'email'      => 'required|email|max:255|unique:users,email,'.(int)$this->route('users'),
+                    'name'  => 'required|max:20|alpha-dash|unique:users,name,' . (int) $this->route('users'),
+                    'email' => 'required|email|max:255|unique:users,email,' . (int) $this->route('users'),
                     'password'   => 'confirmed|min:6',
                     'profile_id' => 'required|numeric',
                 ];
