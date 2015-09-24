a30121a47778aba28c4209425b2650c45615397f
slayerfat <slayerfat@gmail.com>
2015-09-24 15:47:34 -0430

    Caimaneitor: IMPRUVD :see_no_evil:

--- src/app/Mamarrachismo/Caimaneitor/Facades/Caimaneitor.php
+++ src/app/Mamarrachismo/Caimaneitor/Facades/Caimaneitor.php
@@ -1,10 +1,26 @@
-<?php namespace PCI\Mamarrachismo\Caimaneitor\Facades;
+<?php
+
+/**
+ * @link https://www.reddit.com/r/ayylmao/
+ */
+
+namespace PCI\Mamarrachismo\Caimaneitor\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Caimaneitor
+ * @package PCI\Mamarrachismo\Caimaneitor\Facades
+ * @author Alejandro Granadillo <slayerfat@gmail.com>
+ * @link https://github.com/slayerfat/sistemaPCI Repositorio en linea.
+ */
 class Caimaneitor extends Facade
 {
 
+    /**
+     * Nothing to see here, move along.
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'caimaneitor';
--- src/app/Mamarrachismo/Caimaneitor/Interfaces/CaimanizerInterface.php
+++ src/app/Mamarrachismo/Caimaneitor/Interfaces/CaimanizerInterface.php
@@ -1,14 +1,33 @@
-<?php namespace PCI\Mamarrachismo\Caimaneitor\Interfaces;
+<?php
 
+/**
+ * @link https://www.reddit.com/r/ayylmao/
+ */
+
+namespace PCI\Mamarrachismo\Caimaneitor\Interfaces;
+
+/**
+ * Interface CaimanizerInterface
+ * @package PCI\Mamarrachismo\Caimaneitor\Interfaces
+ * @author Alejandro Granadillo <slayerfat@gmail.com>
+ */
 interface CaimanizerInterface
 {
 
     /**
+     * Regresa una caimanizacion.
+     * Inspired by Illuminate\Foundation\Inspiring
+     * A very Inspiring class.
+     * Alejandro Granadillo made this commit from Caracas. (45° Celcius)
      * @return string
      */
     public static function caimanais();
 
     /**
+     * Regresa la locacion del caimanismo.
+     * Inspired by Illuminate\Foundation\Inspiring
+     * A very Inspiring class.
+     * Alejandro Granadillo made this commit from Caracas. (45° Celcius)
      * @return string
      */
     public static function locaishon();
