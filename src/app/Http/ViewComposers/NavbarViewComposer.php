11f52803c449cba8284d29c5703ac4a99b04f601
slayerfat <slayerfat@gmail.com>
2015-09-26 19:18:24 -0430

    mejorado navbar.
    chequea al usuario y brinda links segun el perfil.

--- src/app/Http/ViewComposers/NavbarViewComposer.php
+++ src/app/Http/ViewComposers/NavbarViewComposer.php
@@ -26,6 +26,21 @@ class NavbarViewComposer
     private $icon;
 
     /**
+     * Los enlaces en el navbar que contiene funcionalidad comun
+     * para todos los usuarios independientemente del perfil.
+     * @var array
+     */
+    private $basicLinks = ['petitions'];
+
+    /**
+     * Los enlaces en navbar que contienen funcionalidad
+     * completa solo para el administrador del sistema,
+     * el resto de los usuarios lo veran incompleto.
+     * @var array
+     */
+    private $augmentedLinks = ['notes', 'items', 'depots'];
+
+    /**
      * Genera una nueva instancia de este composer
      * con la implementacion de Guard.
      * @param Guard $auth
@@ -66,43 +81,190 @@ class NavbarViewComposer
      */
     private function getLeftLinks()
     {
-        $links = [
-            [
-                'link'  => '#',
-                'title' => $this->icon->create(trans('models.petitions.fa-icon'))
-                    . trans('models.petitions.plural')
-            ],
-            [
-                'link'  => '#',
-                'title' => $this->icon->create(trans('models.notes.fa-icon'))
-                    . trans('models.notes.plural')
-            ],
-            [
-                $this->icon->create('archive') . 'Items',
-                [
-                    [
-                        'link'  => '#',
-                        'title' => $this->icon->create('plus-circle') . 'Crear'
-                    ],
-                    [
-                        'link'  => '#',
-                        'title' => $this->icon->create('eye') . 'Consultar'
-                    ],
-                    'divider',
-                    [
-                        'link'  => '#',
-                        'title' => $this->icon->create('plus') . '...'
-                    ],
-                ]
-            ],
-        ];
+        // si no hay usuario no se devuelven enlaces,
+        // es decir, no veran enlaces.
+        if (is_null($this->user)) {
+            return [];
+        }
+
+        // estos son los enlaces que contienen todas las caracteristicas
+        // disponibles y que todos los usuarios pueden manipular.
+        $links = $this->makeBasicLinks();
+
+        // estos son los enlaces que solo los administradores tienen
+        // acceso completo, los usuarios tienen acceso limitado.
+        $links = $this->makeAugmentedLinks($links);
 
         // enlaces de mantenimiento (administrador)
-        if ($this->user && $this->user->isAdmin()) {
+        if ($this->isUserAdmin()) {
             return $this->makeAdminLinks($links);
         }
 
         return $links;
+    }
+
+    /**
+     * Genera los links que todos los perfiles del
+     * sistema pueden manipular a plenitud, es
+     * decir, pueden crear, consultar, etc.
+     * @return array[]
+     */
+    private function makeBasicLinks()
+    {
+        $links = [];
+
+        // por cada link normal no interesa un
+        // dropdown con acciones basicas.
+        foreach ($this->basicLinks as $resource) {
+            $links[] = $this->dropdownLinks($resource);
+        }
+
+        return $links;
+    }
+
+    /**
+     * ESte arreglo genera todos los enlaces disponibles por
+     * cada recurso, lo hace en forma de dropdown.
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @return array<string, array>
+     */
+    private function dropdownLinks($resource)
+    {
+        return [
+            $this->icon->create(trans("models.{$resource}.fa-icon"))
+            . trans("models.{$resource}.plural"),
+            [
+                $this->innerArrayCreate($resource),
+                $this->innerArrayIndex($resource),
+                'divider',
+                $this->innerArrayTodo($resource),
+            ]
+        ];
+    }
+
+    /**
+     * Genera un enlace que apunta a crear un nuevo recurso.
+     * Genera un array con informacion valida para los enlaces de tipo
+     * dropdown generados por esta clase, es decir el genera el
+     * array interno que necesita el array padre.
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @param string $icon El icono por defecto de este enlace.
+     * @return array<string, string>
+     */
+    private function innerArrayCreate($resource, $icon = 'plus-circle')
+    {
+        return [
+            'link'  => $this->getRoute("{$resource}.create"),
+            'title' => $this->icon->create($icon) . 'Crear'
+        ];
+    }
+
+    /**
+     * Genera un enlace de placeholder.
+     * Genera un array con informacion valida para los enlaces de tipo
+     * dropdown generados por esta clase, es decir el genera el
+     * array interno que necesita el array padre.
+     * Regresa el enlace generado por route().
+     * Si falla atrapa la excepcion.
+     * @param string $route la direccion: 'users.index'
+     * @return string
+     */
+    private function getRoute($route)
+    {
+        try {
+            return route($route);
+        } catch (\InvalidArgumentException $e) {
+            return "#{$route}.failed";
+        }
+    }
+
+    /**
+     * Genera un enlace que apunta a el listado general de un recurso.
+     * Genera un array con informacion valida para los enlaces de tipo
+     * dropdown generados por esta clase, es decir el genera el
+     * array interno que necesita el array padre.
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @param string $icon El icono por defecto de este enlace.
+     * @return array<string, string>
+     */
+    private function innerArrayIndex($resource, $icon = 'eye')
+    {
+        return [
+            'link'  => $this->getRoute("{$resource}.index"),
+            'title' => $this->icon->create($icon) . 'Consultar'
+        ];
+    }
+
+    /**
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @return array<string, string>
+     */
+    private function innerArrayTodo($resource)
+    {
+        return [
+            'link'  => '#',
+            'title' => $this->icon->create('plus') . "... {$resource}"
+        ];
+    }
+
+    /**
+     * Solo los administradores tienen acceso a todas las
+     * caracteristicas de estos enlaces, el resto de
+     * los usuarios lo tienen de forma limitada.
+     * @param array $links los links son array de arrays
+     * @return array
+     */
+    private function makeAugmentedLinks($links)
+    {
+        // como debemos generar los links pero chequeando el
+        // perfil del usuario, entonces por cada enlace
+        // ejecutamos el metodo checkUserProfile
+        foreach ($this->augmentedLinks as $resource) {
+            $links[] = $this->checkUserProfile($resource);
+        }
+
+        return $links;
+    }
+
+    /**
+     * Genera los links segun el perfil del usuario.
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @return array
+     */
+    private function checkUserProfile($resource)
+    {
+        // si el usuario es administrador se generan
+        // todos los links normales en un dropdown.
+        if ($this->isUserAdmin()) {
+            return $this->dropdownLinks($resource);
+        }
+
+        // de lo contrario se le muestra un link normal.
+        return $this->plainLink($resource);
+    }
+
+    /**
+     * Chequea si el usuario es administrador.
+     * @return bool
+     */
+    private function isUserAdmin()
+    {
+        return $this->user && $this->user->isAdmin();
+    }
+
+    /**
+     * Genera un array que simboliza enlaces basicos en el navbar,
+     * estos apuntan al index del recurso ej: 'users.index'
+     * @param string $resource El recurso ej: 'users' o 'items'.
+     * @return array<string, string>
+     */
+    private function plainLink($resource)
+    {
+        return [
+            'link'  => $this->getRoute("{$resource}.index"),
+            'title' => $this->icon->create(trans("models.{$resource}.fa-icon"))
+                . trans("models.{$resource}.index")
+        ];
     }
 
     /**
@@ -116,68 +278,20 @@ class NavbarViewComposer
         $links[] = [
             $this->icon->create('wrench') . 'Mant.',
             [
-                [
-                    'link'  => route('users.index'),
-                    'title' => $this->icon->create(trans('models.users.fa-icon'))
-                        . trans('models.users.index')
-                ],
-                [
-                    'link'  => route('profiles.index'),
-                    'title' => $this->icon->create(trans('models.profiles.fa-icon'))
-                        . trans('models.profiles.index')
-                ],
-                [
-                    'link'  => route('genders.index'),
-                    'title' => $this->icon->create(trans('models.genders.fa-icon'))
-                        . trans('models.genders.index')
-                ],
-                [
-                    'link'  => route('depts.index'),
-                    'title' => $this->icon->create(trans('models.depts.fa-icon'))
-                        . trans('models.depts.index')
-                ],
-                [
-                    'link'  => route('nats.index'),
-                    'title' => $this->icon->create(trans('models.nats.fa-icon'))
-                        . trans('models.nats.index')
-                ],
+                $this->plainLink('users'),
+                $this->plainLink('profiles'),
+                $this->plainLink('genders'),
+                $this->plainLink('depts'),
+                $this->plainLink('nats'),
                 'divider',
-                [
-                    'link'  => route('cats.index'),
-                    'title' => $this->icon->create(trans('models.cats.fa-icon'))
-                        . trans('models.cats.index')
-                ],
-                [
-                    'link'  => route('subCats.index'),
-                    'title' => $this->icon->create(trans('models.subCats.fa-icon'))
-                        . trans('models.subCats.index')
-                ],
-                [
-                    'link'  => route('itemTypes.index'),
-                    'title' => $this->icon->create(trans('models.itemTypes.fa-icon'))
-                        . trans('models.itemTypes.index')
-                ],
+                $this->plainLink('cats'),
+                $this->plainLink('subCats'),
+                $this->plainLink('itemTypes'),
                 'divider',
-                [
-                    'link'  => route('makers.index'),
-                    'title' => $this->icon->create(trans('models.makers.fa-icon'))
-                        . trans('models.makers.index')
-                ],
-                [
-                    'link'  => route('movementTypes.index'),
-                    'title' => $this->icon->create(trans('models.movementTypes.fa-icon'))
-                        . trans('models.movementTypes.index')
-                ],
-                [
-                    'link'  => route('noteTypes.index'),
-                    'title' => $this->icon->create(trans('models.noteTypes.fa-icon'))
-                        . trans('models.noteTypes.index')
-                ],
-                [
-                    'link'  => route('petitionTypes.index'),
-                    'title' => $this->icon->create(trans('models.petitionTypes.fa-icon'))
-                        . trans('models.petitionTypes.index')
-                ],
+                $this->plainLink('makers'),
+                $this->plainLink('movementTypes'),
+                $this->plainLink('noteTypes'),
+                $this->plainLink('petitionTypes'),
             ]
         ];
 
@@ -229,10 +343,16 @@ class NavbarViewComposer
 
     /**
      * Regresa el formulario necesario para el navbar.
-     * @return string
+     * @return string el string con informacion HTML.
      */
     private function getSearchBox()
     {
+        // si el usuario no tiene sesion no
+        // muestra la barra de busqueda.
+        if (is_null($this->user)) {
+            return '';
+        }
+
         return '<form class="navbar-form navbar-left hidden-sm" role="search">
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Indique su busqueda">
