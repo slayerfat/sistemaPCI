51c4e07a088bacc2e96fd6970b06996886877b13
slayerfat <slayerfat@gmail.com>
2015-09-26 19:16:57 -0430

    ajustado navbar a fluid.
    basicamente para añadir enlaces adicionales.

--- src/resources/views/partials/navbar.blade.php
+++ src/resources/views/partials/navbar.blade.php
@@ -4,5 +4,6 @@ Navbar::withBrand(trans('defaults.appName'), route('index'))
     ->withContent(Navigation::links($links))
     ->withContent($searchBox)
     ->withContent(Navigation::links($rightLinks)->right())
+    ->fluid()
 
 !!}
