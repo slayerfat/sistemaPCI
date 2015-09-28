9a928a3b1b7e5787f2f8d752e158384d4e6f4ba0
slayerfat <slayerfat@gmail.com>
2015-09-27 20:24:36 -0430

    cs asqueroso

--- src/resources/assets/js/deleteResourceFromAnchor.js
+++ src/resources/assets/js/deleteResourceFromAnchor.js
@@ -1,12 +1,9 @@
-function deleteResourceFromAnchor(id)
-{
-    if (!id)
-    {
+function deleteResourceFromAnchor(id) {
+    if (!id) {
         console.log('error, no id.');
         return null;
     }
-    if (confirm('Esta accion no se puede deshacer.'))
-    {
+    if (confirm('Esta accion no se puede deshacer.')) {
         document.getElementById(id).submit();
         return true;
     }
