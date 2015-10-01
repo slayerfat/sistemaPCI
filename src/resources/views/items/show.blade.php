28f26eb093d25980f8921387ff9695b47cff7955
slayerfat <slayerfat@gmail.com>
2015-09-30 21:54:20 -0430

    ajustes iniciales en vista

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -2,13 +2,48 @@
 
 @section('content')
     <div class="container">
-        <h1>
-            Item {{$item}}
+        <div class="col-sm-8">
+            <h1>
+                {{$item->desc}}
 
-            @include(
-                'partials.buttons.edit-delete',
-                ['resource' => 'items', 'id' => $item->id]
-            )
-        </h1>
+                <hr/>
+
+                @include(
+                    'partials.buttons.edit-delete',
+                    ['resource' => 'items', 'id' => $item->id]
+                )
+            </h1>
+
+            <hr/>
+
+            <h2>
+                En {!!link_to_route('subCats.show', $item->subCategory->desc, $item->subCategory->slug)!!}</h2>
+
+            <div class="row">
+                <div class="col-xs-6">
+                    <h4>
+                        Tipo:
+                    </h4>
+
+                    <p>
+                        {!!link_to_route('itemTypes.show', $item->type->desc, $item->type->slug)!!}
+                    </p>
+                </div>
+
+                <div class="col-xs-6">
+                    <h4>
+                        Fabricante:
+                    </h4>
+
+                    <p>
+                        {!!link_to_route('makers.show', $item->maker->desc, $item->maker->slug)!!}
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <h1>Ultimos Movimientos</h1>
+        </div>
     </div>
 @stop
