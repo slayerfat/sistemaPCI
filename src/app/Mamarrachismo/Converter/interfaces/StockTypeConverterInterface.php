5ae88829ece8917500c746759bb10d336ab58a07
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-11 13:35:51 -0430

    el convertidor no depende del item en cada metodo sino en construct

--- src/app/Mamarrachismo/Converter/interfaces/StockTypeConverterInterface.php
+++ src/app/Mamarrachismo/Converter/interfaces/StockTypeConverterInterface.php
@@ -1,29 +1,31 @@
 <?php namespace PCI\Mamarrachismo\Converter\interfaces;
 
-use PCI\Models\Item;
-
 interface StockTypeConverterInterface
 {
 
     /**
-     * @param $type
-     * @param $amount
-     * @param \PCI\Models\Item $item
-     * @return \PCI\Models\Item
-     */
-    public function validate($type, $amount, Item $item = null);
-
-    /**
-     * @param $type
-     * @param $amount
-     * @param \PCI\Models\Item $item
-     * @return \PCI\Models\Item
-     */
-    public function convert($type, $amount, Item $item = null);
-
-    /**
-     * @param \PCI\Models\Item $item
+     * Valida que la cantidad solicitada sea apropiada
+     * al stock que posea determinado Item.
+     *
+     * @param int $type El tipo de stock/cantidad
      * @return bool
      */
-    public function isConvertible(Item $item = null);
+    public function validate($type);
+
+    /**
+     * Convierte o trasforma la cantidad solicitada a
+     * una cantidad compatible con el tipo de stock.
+     *
+     * @param int $type   El tipo stock/cantidad
+     * @param int $amount la cantidad del pedido/nota/movimiento
+     * @return int el numero transformado
+     */
+    public function convert($type, $amount);
+
+    /**
+     * Chequea si el tipo es convertible segun reglas.
+     *
+     * @return bool
+     */
+    public function isConvertible();
 }
