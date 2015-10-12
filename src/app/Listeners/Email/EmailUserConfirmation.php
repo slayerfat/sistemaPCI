f5d77aced12140d31ee22af4a4e6554c84895b51
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-12 14:29:41 -0430

    carpinteria

--- src/app/Listeners/Email/EmailUserConfirmation.php
+++ src/app/Listeners/Email/EmailUserConfirmation.php
@@ -3,13 +3,15 @@
 namespace PCI\Listeners\Email;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Message;
 use PCI\Events\NewUserRegistration;
 
 /**
  * Class EmailUserConfirmation
+ *
  * @package PCI\Listeners
- * @author Alejandro Granadillo <slayerfat@gmail.com>
- * @link https://github.com/slayerfat/sistemaPCI Repositorio en linea.
+ * @author  Alejandro Granadillo <slayerfat@gmail.com>
+ * @link    https://github.com/slayerfat/sistemaPCI Repositorio en linea.
  */
 class EmailUserConfirmation extends AbstractEmailListener implements ShouldQueue
 {
@@ -19,16 +21,20 @@ class EmailUserConfirmation extends AbstractEmailListener implements ShouldQueue
      * necesario para que el usuario pueda
      * confirmar su cuenta por medio
      * de la clave de ocnfirmacion.
-     * @param  NewUserRegistration  $event
+     *
+     * @param  NewUserRegistration $event
      * @return void
      */
     public function handle(NewUserRegistration $event)
     {
         $user = $event->user;
-
         $email = $event->user->email;
 
-        $this->mail->send(['emails.verify', 'emails.verify-plain'], compact('user'), function ($message) use ($email) {
+        $this->mail->send([
+            'emails.verify',
+            'emails.verify-plain',
+        ], compact('user'), function ($message) use ($email) {
+            /** @var Message $message */
             $message->to($email)
                 ->subject('sistemaPCI: por favor verifique su cuenta.');
         });
