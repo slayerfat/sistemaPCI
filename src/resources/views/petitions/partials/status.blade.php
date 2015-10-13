ee82296f3df1f7e09de8a7a95d39477985636b45
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-13 16:03:45 -0430

    añadido mensaje del sistema

--- src/resources/views/petitions/partials/status.blade.php
+++ src/resources/views/petitions/partials/status.blade.php
@@ -17,6 +17,10 @@ if (is_null($petition->status)) {
     data-status="{{ $status }}">
 </h3>
 
+<div class="message-float alert" style="display:none;">
+    <p></p>
+</div>
+
 @section('js-buttons')
     <script type="text/javascript">
         $(function () {
@@ -24,8 +28,11 @@ if (is_null($petition->status)) {
             var url = $statusElement.data('route');
 
             var status = {
+                previousStatus: '',
 
                 status: $statusElement.data('status'),
+
+                $msg: $('.message-float'),
 
                 html: {
                     green: 'Estado: ' + '<span class="green">Aprobado ' +
@@ -51,6 +58,11 @@ if (is_null($petition->status)) {
                     '</span>'
                 },
 
+                /**
+                 * necesitamos determinar el estatus del pedido
+                 * para saber cual html de los disponibles debemos añadir.
+                 * @returns {*}
+                 */
                 start: function () {
                     if (this.status == null) {
                         return $statusElement.append(this.html.yellow);
@@ -61,22 +73,85 @@ if (is_null($petition->status)) {
                     return $statusElement.append(this.html.red);
                 },
 
+                /**
+                 * Cuando el usuario le da click a aprobar o rechazar, enviamos
+                 * una peticion ajax al servidor y cambiamos la vista acordemente.
+                 * @param string url
+                 * @param bool status
+                 */
                 change: function (url, status) {
+                    this.previousStatus = this.status;
                     this.status = status;
+
+                    var self = this;
 
                     $.ajax({
                         url: url,
                         type: 'POST',
                         dataType: 'json',
                         data: {status: status}
-                    }).done(function () {
-                        console.log("success");
+                    }).done(function (data) {
+                        var element;
+
+                        if (status == null && data.status) {
+                            element = self.html.yellow;
+                        } else {
+                            element = data.status ? self.html.green : self.html.red;
+                        }
+
+                        // esperamos 250 milsegs, luego limpiamos
+                        // el contenido html y lo cambiamos por el elemento.
+                        $statusElement.animate({opacity: 0}, 250, 'linear', function () {
+                            $statusElement.empty().html(element);
+                        }).animate({opacity: 1}, 250, 'linear');
+
+                        // creamos un mensaje segun este metodo.
+                        self.createMessage(
+                                'Cambios realizados correctamente. ' +
+                                '<a href="#" id="message-undo">deshacer</a>',
+                                'alert alert-success'
+                        );
                     }).fail(function () {
-                        console.log("error");
+                        self.createMessage(
+                                'Error Desconocido en el servidor.',
+                                'alert alert-warning'
+                        );
+                    });
+                },
+
+                /**
+                 * Creamos un mensaje que flotara en
+                 * la vista con la informacion suministrada.
+                 * @param string msg el mensaje para el usuario
+                 * @param string classes las clases para el css
+                 */
+                createMessage: function (msg, classes) {
+                    var self = this;
+
+                    this.$msg.removeClass('alert alert-danger alert-success')
+                            .addClass(classes)
+                            .show()
+                            .children()
+                            .html(msg);
+
+                    this.$msg.children().children().click(function () {
+                        status.change(url, self.previousStatus);
+                    });
+
+                    setTimeout(this.toggleMessage, 10000);
+                },
+
+                /**
+                 * cambia el estado del mensaje ya creado.
+                 */
+                toggleMessage: function () {
+                    return status.$msg.fadeToggle(1000, function () {
+                        $(this).children().empty();
                     });
                 }
             };
 
+            // aqui empieza el mamarrachismo.
             status.start();
 
             // http://laravel.com/docs/master/routing#csrf-x-csrf-token
