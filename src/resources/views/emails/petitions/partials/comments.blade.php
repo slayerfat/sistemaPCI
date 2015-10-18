82dce438cdb8bc15bc2cc1ab581a4fe8a084453a
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-18 11:12:22 -0430

    añadido margen al comentario

--- src/resources/views/emails/petitions/partials/comments.blade.php
+++ src/resources/views/emails/petitions/partials/comments.blade.php
@@ -1,3 +1,3 @@
-<p style="background-color: #f0f0f0; padding: 1em; margin: 0.3em 0;">
+<p style="background-color: #f0f0f0; padding: 1em; margin: 0.3em 0;max-width: 90%">
     {{ $petition->comments }}
 </p>
