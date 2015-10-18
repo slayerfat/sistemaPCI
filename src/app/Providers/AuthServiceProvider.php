5bfc4b66381a1b9985d8280fc75310faa399a0dc
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-17 20:03:15 -0430

    añadido notas al service provider

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -4,10 +4,12 @@ use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use PCI\Models\Address;
 use PCI\Models\Employee;
+use PCI\Models\Note;
 use PCI\Models\Petition;
 use PCI\Models\User;
 use PCI\Models\WorkDetail;
 use PCI\Policies\Item\PetitionPolicy;
+use PCI\Policies\Note\NotePolicy;
 use PCI\Policies\User\AddressPolicy;
 use PCI\Policies\User\EmployeePolicy;
 use PCI\Policies\User\UserPolicy;
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * The policy mappings for the application.
+     *
      * @var array
      */
     protected $policies = [
@@ -25,11 +28,13 @@ class AuthServiceProvider extends ServiceProvider
         Employee::class   => EmployeePolicy::class,
         Address::class    => AddressPolicy::class,
         WorkDetail::class => WorkDetailPolicy::class,
-        Petition::class   => PetitionPolicy::class
+        Note::class       => NotePolicy::class,
+        Petition::class   => PetitionPolicy::class,
     ];
 
     /**
      * Register any application authentication / authorization services.
+     *
      * @param  \Illuminate\Contracts\Auth\Access\Gate $gate
      * @return void
      */
