3d592e6230c4140fa333d58e1b4dcd3b57159e89
Alejandro Granadillo <slayerfat@gmail.com>
2015-10-19 21:12:27 -0430

    añadido repo de notas para el controlador de notas

--- src/app/Http/Controllers/Api/Note/NotesController.php
+++ src/app/Http/Controllers/Api/Note/NotesController.php
@@ -5,6 +5,7 @@ namespace PCI\Http\Controllers\Api\Note;
 use PCI\Http\Controllers\Controller;
 use PCI\Http\Controllers\Traits\RespondsToChangeStatus;
 use PCI\Http\Requests;
+use PCI\Repositories\Interfaces\Note\NoteRepositoryInterface;
 use Response;
 
 /**
@@ -18,6 +19,23 @@ class NotesController extends Controller
 {
 
     use RespondsToChangeStatus;
+
+    /**
+     * La implementacion de este repositorio.
+     *
+     * @var \PCI\Repositories\Interfaces\Note\NoteRepositoryInterface
+     */
+    private $repo;
+
+    /**
+     * Genera una nueva instancia de este api.
+     *
+     * @param NoteRepositoryInterface $repo
+     */
+    public function __construct(NoteRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
 
     /**
      * Genera un nuevo PDF relacionada con la nota.
