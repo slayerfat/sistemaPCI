8b1aa3d392ea326b69825f50f01a6d27ba312980
Alejandro Granadillo <slayerfat@gmail.com>
2015-11-01 20:28:11 -0430

    :smirk: abstraida logica de cambio de boton, a clase

--- src/resources/views/petitions/partials/show-buttons.blade.php
+++ src/resources/views/petitions/partials/show-buttons.blade.php
@@ -7,7 +7,6 @@
                 ->asLinkTo('#')
                 ->withAttributes([
                     'id'        => 'petition-approval-request',
-                    'data-sent' => 'false',
                     'data-url'  => route('api.petitions.approvalRequest', $petition->id),
                 ])->withIcon(Icon::create('exclamation-circle'))
 
@@ -22,45 +21,33 @@
 
 @section('js-show-buttons')
     <script type="text/javascript">
-        $('#petition-approval-request').click(function () {
-            var $sent = $(this);
+        var $element = $('#petition-approval-request');
+        var button = new Forms.ButtonSpinner($element);
+        button.sent = false;
+
+        $element.click(function (evt) {
+            evt.preventDefault();
 
             // si la peticion ha sido enviada, se regresa prematuramente.
-            if ($sent.data('sent')) return;
+            if (button.sent) return;
 
             $.ajax({
-                url: $sent.data('url'),
+                url: $element.data('url'),
                 method: 'POST',
                 dataType: 'json',
                 beforeSend: function () {
-                    $('#petition-approval-request')
-                            .removeClass('btn-default')
-                            .addClass('btn-warning')
-                            .find('span').removeClass('fa-exclamation-circle')
-                            .addClass('fa-spinner fa-spin');
+                    button.before('fa-exclamation-circle');
                 },
                 complete: function () {
-                    $('#petition-approval-request')
-                            .removeClass('btn-warning')
-                            .find('span').removeClass('fa-spinner fa-spin');
+                    button.complete();
                 }
             }).success(function (data) {
                 if (data.status == true) {
-                    $('#petition-approval-request')
-                            .removeClass('btn-default')
-                            .addClass('btn-success')
-                            .find('span')
-                            .addClass('fa-check-circle');
-
-                    $sent.data('sent', true);
+                    button.success();
+                    button.sent = true;
                 }
             }).fail(function () {
-                $('#petition-approval-request')
-                        .removeClass('btn-default')
-                        .addClass('btn-danger')
-                        .find('span')
-                        .removeClass('*')
-                        .addClass('fa-times-circle');
+                button.fail();
             })
         })
     </script>
