59ffa3cb91bef9f854ff01e81d4b4bb989fe5787
Alejandro Granadillo <slayerfat@gmail.com>
2015-11-25 14:02:26 -0430

    ajustado footer de los PDF

--- src/resources/views/partials/pdf/footer.blade.php
+++ src/resources/views/partials/pdf/footer.blade.php
@@ -1,4 +1,10 @@
-<footer>
-    <p>Reporte generado: {{ Date::now() }} por {{ Auth::user()->name }}
-        ({{ Auth::user()->email }})</p>
-</footer>
+<script type="text/php">
+    if ( isset($pdf) ) {
+        $width = 36;
+        $height = $pdf->get_height() - 36;
+        $generated = e("Reporte generado: " . Date::now() . " por "
+            . Auth::user()->name . " (" . Auth::user()->email . ")");
+        $font = Font_Metrics::get_font("helvetica", "bold");
+        $pdf->page_text($width, $height, "$generated, Pagina: {PAGE_NUM} of {PAGE_COUNT}", $font, 9);
+    }
+</script>
